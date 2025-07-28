a37d2ccf0415ab4506dead65127354e78b5dca6d
Andres <andres13cajas@gmail.com>
2025-07-27 19:14:16 -0500

    Feat(UPS-006): Implementacion de Dao (Guardar datos) e implementado imple metodos

--- src/main/java/dao/AlgorithmResultadoDAO.java
+++ src/main/java/dao/AlgorithmResultadoDAO.java
@@ -1,4 +1,13 @@
 package dao;
 
+import models.AlgorithmResultado;
+
+import java.util.List;
+
 public interface AlgorithmResultadoDAO {
+    void guardarResultado( AlgorithmResultado resultado );
+
+    List<AlgorithmResultado> obtenerTodos();
+
+    void eliminarTodos();
 }
